b3ecd5d3dbd156d5a12d99558f2dddb53e46b926
Romans Kovalenoks <kovaljonokroman@gmail.com>
2025-01-05 23:06:34 +0200

    Main page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import styles from "./page.module.scss";
+import s from "./page.module.scss";
 import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Hello! This is &quot;Token Portfolio&quot;</h1>
-        <Link className={styles.mainPageLink} href='/portfolio'>
-          Proceed To Portfolio Page
-        </Link>
+    <div className={s.page}>
+      <main className={s.main}>
+        <div className={s.headerContainer}>
+          <h1>Hello! This is &quot;Token Portfolio&quot;</h1>
+          <Link className={s.mainPageLink} href='/portfolio'>
+            Proceed To Portfolio Page
+          </Link>
+        </div>
       </main>
     </div>
   );
